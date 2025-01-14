982bcf9a72ea10c7623e5e9143038d0287f113c0
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2025-01-14 16:25:02 +0000

    (feat) improve documentation and fix test folder

--- src/gws_plate_reader/biolector_xt_parser/standalone_parser_dashboard/standalone_parser_dashboard.py
+++ src/gws_plate_reader/biolector_xt_parser/standalone_parser_dashboard/standalone_parser_dashboard.py
@@ -24,6 +24,40 @@ class BiolectorParserStandaloneClass(Dashboard):
                 short_description="Task to generate a standalone Streamlit dashboard to visualise data from BiolectorXT",
                 style=TypingStyle.community_icon(icon_technical_name="dashboard", background_color="#c3fa7f"))
 class BiolectorParserStandalone(Task):
+    """
+    Generate a standalone dashboard to visualise data from BiolectorXT.
+
+    Check this story for more details: [BiolectorXT](https://constellab.community/stories/4f2d0ea1-8718-430d-8501-2778fbbbedcf/unlock-the-power-of-your-biolector-xt-data-with-constellab)
+    ## How It Works: 
+
+    - ⬆️ Upload your CSV and JSON files.
+    - 🚀 Get redirected to the intuitive Analysis Dashboard.
+    - 🔍 Navigate through data and insights effortlessly.
+
+    ## Tables page
+
+    This page lets you explore raw data for each observer (e.g., biomass).
+    Simply select the wells you want to focus on directly on the microplate view.
+    Selected wells are highlighted in green, and the table dynamically updates to display results for your selection.
+
+    ## Plots page
+
+    Visualize your data with interactive plots. 
+
+    - 📊 View observer data for all wells.
+    - ⚙️ Choose specific observers, adjust time units (hours, minutes, or seconds), and switch between display modes (“Individual Curves” or “Mean”).
+    - 🎯 Select wells on the microplate to focus only on relevant curves.
+    - 📈 Add an error band to the “Mean” plot for enhanced analysis.
+
+    Choosing Mean calculates the average of the selected wells, and you can optionally display an error band to enrich the plot.
+
+    ## Analysis page
+
+    Dive deeper with advanced metrics: 
+
+    - 🔬 Calculate growth rates and maximum absorbance values.
+    - 🧮 Overlay raw data and fitted curves for precise insights.
+    """
 
     input_specs: InputSpecs = InputSpecs()
     output_specs: OutputSpecs = OutputSpecs(

6cb0fc85819d70d31c01ca4dfbfc90695e67065b
Maëva <mbeugin@gencovery.com>
2025-02-12 16:06:19 +0000

    First commit of plate layout

---
+++ src/gws_plate_reader/dashboard_plate_layout/_dashboard_code/main.py
@@ -0,0 +1,228 @@
+import os
+import json
+import streamlit as st
+import pandas as pd
+from streamlit_extras.stylable_container import stylable_container
+
+# thoses variable will be set by the streamlit app
+# don't initialize them, there are create to avoid errors in the IDE
+sources: list
+params: dict
+
+def show_content():
+
+    #Create tabs
+    tab_dict, tab_plate_layout= st.tabs(["Dict", "Plate Layout"])
+
+    with tab_dict:
+        st.header("Fill data")
+
+        length_diff = len(st.session_state.compounds) - len(st.session_state.dilutions)
+        # Add None to the shorter list
+        if length_diff > 0:  # Compounds list is longer
+            st.session_state.dilutions.extend([None] * length_diff)
+        elif length_diff < 0:  # Dilutions list is longer
+            st.session_state.compounds.extend([None] * abs(length_diff))
+
+        #Create the dataframe
+        dict_df = pd.DataFrame(columns = ["Compounds","Dilutions"])
+        dict_df["Compounds"] = st.session_state.compounds
+        dict_df["Dilutions"] = st.session_state.dilutions
+        dict_df["Compounds"] = dict_df["Compounds"].fillna('').astype('str')
+        dict_df["Dilutions"] = dict_df["Dilutions"].fillna('').astype('str')
+
+        edited_dict_df = st.data_editor(dict_df, use_container_width = True, hide_index=True, num_rows="dynamic")
+
+        if st.button("Save data") :
+            st.session_state['compounds']= edited_dict_df["Compounds"].to_list()
+            st.session_state['dilutions']= edited_dict_df["Dilutions"].to_list()
+            file_dict_compounds_path = os.path.join(folder_data,"compounds.json")
+            with open(file_dict_compounds_path, "w") as json_file:
+                json.dump(st.session_state.compounds, json_file, indent=4)
+
+            file_dict_dilutions_path = os.path.join(folder_data,"dilutions.json")
+            with open(file_dict_dilutions_path, "w") as json_file:
+                json.dump(st.session_state.dilutions, json_file, indent=4)
+
+            st.success("Information saved successfully!")
+
+
+    with tab_plate_layout:
+        if st.session_state['well_clicked'] :
+            st.write(f"Fill informations for {', '.join(st.session_state['well_clicked'])}:")
+
+            # Compound and dilution selection
+            selected_compound = st.selectbox(label="Compound", options=st.session_state['compounds'],placeholder="Choose an option", index = None)
+            selected_dilution = st.selectbox(label="Dilution", options=st.session_state['dilutions'],placeholder="Choose an option", index = None)
+
+            # Save information for selected wells
+            if st.button("Save these informations"):
+                if selected_compound is not None or selected_dilution is not None :
+                    for well in st.session_state['well_clicked']:
+                        if well not in st.session_state['plate_layout']:
+                            st.session_state['plate_layout'][well] = {}
+                    if selected_compound is not None :
+                        for well in st.session_state['well_clicked']:
+                            st.session_state['plate_layout'][well]["compound"] = selected_compound
+                    if selected_dilution is not None :
+                        for well in st.session_state['well_clicked']:
+                            st.session_state['plate_layout'][well]["dilution"] = selected_dilution
+
+                # Save the plate layout to a JSON file
+                file_plate_layout_path = os.path.join(folder_data, "plate_layout.json")
+                os.makedirs(os.path.dirname(file_plate_layout_path), exist_ok=True)  # Ensure directory exists
+                with open(file_plate_layout_path, "w") as json_file:
+                    json.dump(st.session_state['plate_layout'], json_file, indent=4)
+                st.success("Information saved successfully!") #TODO :trouver un moyen d'afficher un message de succès parce que là c'est trop vite à cause du rerun
+                st.rerun()
+
+        else :
+            st.write("Please select one well at least.")
+
+
+
+#-------------------------------------------------------------------------------------------#
+
+if not sources:
+    raise Exception("Source paths are not provided.")
+
+folder_data = sources[0].path
+number_wells = params["number_wells"]
+
+# Initialize the session state for clicked wells if it doesn't exist
+if 'well_clicked' not in st.session_state:
+    st.session_state['well_clicked'] = []
+
+files_compounds = [f for f in os.listdir(folder_data) if f.endswith("compounds.json")]
+if files_compounds :
+    # Load the file and display its contents
+    file_path = os.path.join(folder_data, files_compounds[0])
+    with open(file_path, "r") as f:
+        st.session_state.compounds = json.load(f)
+else :
+    # Create a dictionary to store  data
+    if 'compounds' not in st.session_state:
+        st.session_state['compounds'] = []
+
+files_dilutions = [f for f in os.listdir(folder_data) if f.endswith("dilutions.json")]
+if files_dilutions :
+    file_path = os.path.join(folder_data, files_dilutions[0])
+    with open(file_path, "r") as f:
+        st.session_state.dilutions = json.load(f)
+else :
+    # Create a dictionary to store  data
+    if 'dilutions' not in st.session_state:
+        st.session_state['dilutions'] = []
+
+files_plate_layout = [f for f in os.listdir(folder_data) if f.endswith("plate_layout.json")]
+if files_plate_layout :
+    # Load the file and display its contents
+    file_path_plate_layout = os.path.join(folder_data, files_plate_layout[0])
+    with open(file_path_plate_layout, "r") as f:
+        st.session_state.plate_layout = json.load(f)
+else :
+    if "plate_layout" not in st.session_state:
+        st.session_state["plate_layout"] = {}
+
+
+if number_wells == 96:
+    # Define the structure of the 96-well microplate
+    ROWS = 8
+    COLS = 12
+    size_sidebar = 580
+    size_button = 41
+    font_size = 4
+elif number_wells == 48:
+    # Define the structure of the 48-well microplate
+    ROWS = 6
+    COLS = 8
+    size_sidebar = 400
+    size_button = 44
+    font_size = 8
+
+
+# Inject custom CSS to set the width of the sidebar
+st.markdown(
+    f"""
+    <style>
+        section[data-testid="stSidebar"] {{
+            width: {size_sidebar}px !important; /* Set the width to your desired value */
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+with st.sidebar:
+    @st.fragment
+    def fragment_sidebar_function():
+        st.write("Microplate")
+        with stylable_container(key="well_button", css_styles = f"""
+            button{{
+                display: inline-block;
+                width: {size_button}px;  /* Adjust width and height as needed */
+                height: {size_button}px;
+                border-radius: 50%;  /* Make it circular */
+                background-color: #eb969d;  /* Button color */
+                color: black;  /* Text color */
+                text-align: center;
+                line-height: {size_button}px;  /* Center text vertically */
+                font-size: {font_size}px;  /* Text size */
+                padding: 0;  /* Remove padding to avoid extra space */
+                cursor: pointer;
+                text-decoration: none;  /* Remove underline */
+                }}
+            button:active {{
+                position:relative;
+                top:1px;
+                }}
+                """,):
+
+
+
+            # Define the structure of the microplate
+            wells = [[f"{chr(65 + row)}{col + 1}" for col in range(COLS)] for row in range(ROWS)]
+            # Loop over the wells and create a grid of buttons
+            for row in range(ROWS):
+                cols_object = st.columns(COLS)
+                for col in range(COLS):
+                    well = wells[row][col]
+
+                    if well in st.session_state['well_clicked']:
+                        if well in st.session_state['plate_layout'] and st.session_state['plate_layout'][well].get("compound") and st.session_state['plate_layout'][well].get("dilution") :
+                            if cols_object[col].button(f":green[{well}] ✓", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                                st.session_state['well_clicked'].remove(well)
+                                st.rerun(scope="app")
+                        elif well in st.session_state['plate_layout']:
+                            if cols_object[col].button(f":green[{well}]", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                                st.session_state['well_clicked'].remove(well)
+                                st.rerun(scope="app")
+                        else :
+                            if cols_object[col].button(f"**:green[{well}]**", key=well):
+                                st.session_state['well_clicked'].remove(well)
+                                st.rerun(scope="app")
+                    else:
+                        if well in st.session_state['plate_layout'] and st.session_state['plate_layout'][well].get("compound") and st.session_state['plate_layout'][well].get("dilution"):
+                            if cols_object[col].button(f"{well} ✓", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                                st.session_state['well_clicked'].append(well)
+                                st.rerun(scope="app")
+                        elif well in st.session_state['plate_layout']:
+                            if cols_object[col].button(well, key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                                st.session_state['well_clicked'].append(well)
+                                st.rerun(scope="app")
+                        else:
+                            if cols_object[col].button(well, key=well):
+                                st.session_state['well_clicked'].append(well)
+                                st.rerun(scope="app")
+
+            st.write(f"All the wells clicked are: {', '.join(st.session_state['well_clicked'])}")
+
+    fragment_sidebar_function()
+
+    def reset_wells():
+        st.session_state['well_clicked'] = []
+    # Add the reset button
+    st.button("Reset Wells", on_click=reset_wells)
+
+show_content()
---
+++ src/gws_plate_reader/dashboard_plate_layout/streamlit_generator.py
@@ -0,0 +1,49 @@
+
+import os
+
+from gws_core import (ConfigParams, ConfigSpecs, OutputSpec, OutputSpecs, StreamlitResource, Task, TaskInputs, TaskOutputs, task_decorator,
+                      dashboard_decorator, Dashboard, DashboardType, Folder, TypingStyle, StrParam)
+
+@dashboard_decorator("GenerateDashboardPlateLayout", dashboard_type=DashboardType.STREAMLIT)
+class GenerateDashboardPlateLayout(Dashboard):
+
+    # retrieve the path of the app folder, relative to this file
+    # the dashboard code folder starts with a underscore to avoid being loaded when the brick is loaded
+    def get_app_folder_path(self):
+        return os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "_dashboard_code"
+        )
+
+@task_decorator("StreamlitGeneratorPlateLayout", human_name="Generate dashboard to fill Plate Layout",
+                short_description="Task to generate a custom Streamlit dashboard to fill Plate Layout",
+                style=TypingStyle.community_icon(icon_technical_name="matrix", background_color="#60a182"))
+class StreamlitGeneratorPlateLayout(Task):
+
+    config_specs: ConfigSpecs = {
+        'number_wells': StrParam(allowed_values=["48", "96"],
+                                human_name="Number of wells",
+                                default_value="48",optional=False,
+                                  short_description="The number of wells of the microplate"),
+
+    }
+
+    output_specs: OutputSpecs = OutputSpecs({'streamlit_app': OutputSpec( StreamlitResource, human_name="Microplate dashboard")})
+
+    def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
+        # Get the number of wells
+        number_wells: int = int(params.get_value('number_wells'))
+
+        # build the streamlit resource with the code and the resources
+        streamlit_resource = StreamlitResource()
+
+        folder_data: Folder = Folder(self.create_tmp_dir())
+        folder_data.name = "Data"
+        streamlit_resource.add_resource(folder_data, create_new_resource=True)
+
+
+        # set dashboard reference
+        streamlit_resource.set_dashboard(GenerateDashboardPlateLayout())
+        streamlit_resource.set_params({"number_wells": number_wells})
+
+        return {'streamlit_app': streamlit_resource}

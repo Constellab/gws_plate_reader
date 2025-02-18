807ff526012b1ae90159642614fdb21ce5e52944
Maëva <mbeugin@gencovery.com>
2025-02-18 12:03:25 +0000

    Add button to generate plate layout resource

--- src/gws_plate_reader/dashboard_plate_layout/_dashboard_code/main.py
+++ src/gws_plate_reader/dashboard_plate_layout/_dashboard_code/main.py
@@ -3,6 +3,7 @@ import json
 import streamlit as st
 import pandas as pd
 from streamlit_extras.stylable_container import stylable_container
+from gws_core import File, ResourceOrigin, ResourceModel, Settings, FrontService, JSONImporter
 # thoses variable will be set by the streamlit app
 # don't initialize them, there are create to avoid errors in the IDE
 sources: list
@@ -12,92 +13,115 @@ params: dict
 if "success_message" not in st.session_state:
     st.session_state["success_message"] = None
 
+def reset_wells():
+    st.session_state['well_clicked'] = []
+    
 # Function to display success message after rerun
 def show_success_message():
     if st.session_state["success_message"]:
         st.success(st.session_state["success_message"])
-        st.session_state["success_message"] = None  # Clear message after displaying
+        # Clear message after displaying
+        st.session_state["success_message"] = None
 
 def show_content():
 
-    #Create tabs
-    tab_dict, tab_plate_layout= st.tabs(["Dict", "Plate Layout"])
+    # Create tabs
+    tab_dict, tab_plate_layout = st.tabs(["Dict", "Plate Layout"])
 
     with tab_dict:
         st.header("Fill data")
 
-        length_diff = len(st.session_state.compounds) - len(st.session_state.dilutions)
+        length_diff = len(st.session_state.compounds) - \
+            len(st.session_state.dilutions)
         # Add None to the shorter list
         if length_diff > 0:  # Compounds list is longer
             st.session_state.dilutions.extend([None] * length_diff)
         elif length_diff < 0:  # Dilutions list is longer
             st.session_state.compounds.extend([None] * abs(length_diff))
 
-        #Create the dataframe
-        dict_df = pd.DataFrame(columns = ["Compounds","Dilutions"])
+        # Create the dataframe
+        dict_df = pd.DataFrame(columns=["Compounds", "Dilutions"])
         dict_df["Compounds"] = st.session_state.compounds
         dict_df["Dilutions"] = st.session_state.dilutions
         dict_df["Compounds"] = dict_df["Compounds"].fillna('').astype('str')
         dict_df["Dilutions"] = dict_df["Dilutions"].fillna('').astype('str')
 
-        edited_dict_df = st.data_editor(dict_df, use_container_width = True, hide_index=True, num_rows="dynamic")
+        edited_dict_df = st.data_editor(
+            dict_df, use_container_width=True, hide_index=True, num_rows="dynamic")
 
-        if st.button("Save data") :
-            st.session_state['compounds']= edited_dict_df["Compounds"].to_list()
-            st.session_state['dilutions']= edited_dict_df["Dilutions"].to_list()
-            file_dict_compounds_path = os.path.join(folder_data,"compounds.json")
+        if st.button("Save data"):
+            st.session_state['compounds'] = edited_dict_df["Compounds"].to_list()
+            st.session_state['dilutions'] = edited_dict_df["Dilutions"].to_list()
+            file_dict_compounds_path = os.path.join(
+                folder_data, "compounds.json")
             with open(file_dict_compounds_path, "w") as json_file:
                 json.dump(st.session_state.compounds, json_file, indent=4)
 
-            file_dict_dilutions_path = os.path.join(folder_data,"dilutions.json")
+            file_dict_dilutions_path = os.path.join(
+                folder_data, "dilutions.json")
             with open(file_dict_dilutions_path, "w") as json_file:
                 json.dump(st.session_state.dilutions, json_file, indent=4)
 
             st.success("Information saved successfully! ✅")
 
-
     with tab_plate_layout:
+        # Add the button to generate plate layout
+        if st.button("Generate plate layout ressource", icon = ":material/note_add:"):
+            path_temp = os.path.join(os.path.abspath(os.path.dirname(__file__)), Settings.make_temp_dir())
+            full_path = os.path.join(path_temp, f"Plate_layout.json")
+            plate_layout: File = File(full_path)
+            # Convert dict to JSON string
+            json_str = json.dumps(st.session_state['plate_layout'], indent=4)
+            plate_layout.write(json_str)
+            #Import the resource as JSONDict
+            plate_layout_json_dict = JSONImporter.call(plate_layout)
+            plate_layout_resource = ResourceModel.save_from_resource(
+                plate_layout_json_dict, ResourceOrigin.UPLOADED, flagged=True)
+            st.success(f"Resource created! ✅ You can find it here : {FrontService.get_resource_url(plate_layout_resource.id)}")
         if not st.session_state['compounds'] and not st.session_state['dilutions']:
             st.warning("Please fill in at least one compound or dilution")
         else:
-            if st.session_state['well_clicked'] :
-                st.write(f"Fill informations for {', '.join(st.session_state['well_clicked'])}:")
+            if st.session_state['well_clicked']:
+                st.write(
+                    f"Fill informations for {', '.join(st.session_state['well_clicked'])}:")
 
                 # Compound and dilution selection
-                selected_compound = st.selectbox(label="Compound", options=st.session_state['compounds'],placeholder="Choose an option", index = None)
-                selected_dilution = st.selectbox(label="Dilution", options=st.session_state['dilutions'],placeholder="Choose an option", index = None)
+                selected_compound = st.selectbox(
+                    label="Compound", options=st.session_state['compounds'], placeholder="Choose an option", index=None)
+                selected_dilution = st.selectbox(
+                    label="Dilution", options=st.session_state['dilutions'], placeholder="Choose an option", index=None)
 
                 # Save information for selected wells
                 if st.button("Save these informations"):
-                    if selected_compound is not None or selected_dilution is not None :
+                    if selected_compound is not None or selected_dilution is not None:
                         for well in st.session_state['well_clicked']:
                             if well not in st.session_state['plate_layout']:
                                 st.session_state['plate_layout'][well] = {}
-                        if selected_compound is not None :
+                        if selected_compound is not None:
                             for well in st.session_state['well_clicked']:
                                 st.session_state['plate_layout'][well]["compound"] = selected_compound
-                        if selected_dilution is not None :
+                        if selected_dilution is not None:
                             for well in st.session_state['well_clicked']:
                                 st.session_state['plate_layout'][well]["dilution"] = selected_dilution
 
                     # Save the plate layout to a JSON file
-                    file_plate_layout_path = os.path.join(folder_data, "plate_layout.json")
-                    os.makedirs(os.path.dirname(file_plate_layout_path), exist_ok=True)  # Ensure directory exists
+                    file_plate_layout_path = os.path.join(
+                        folder_data, "plate_layout.json")
+                    os.makedirs(os.path.dirname(file_plate_layout_path),
+                                exist_ok=True)  # Ensure directory exists
                     with open(file_plate_layout_path, "w") as json_file:
-                        json.dump(st.session_state['plate_layout'], json_file, indent=4)
+                        json.dump(
+                            st.session_state['plate_layout'], json_file, indent=4)
 
                     st.session_state["success_message"] = "Information saved successfully! ✅"
                     st.rerun()
                 show_success_message()  # Show success message if it exists
 
-
-            else :
+            else:
                 st.warning("Please select one well at least.")
 
 
-
-#-------------------------------------------------------------------------------------------#
-
+# -------------------------------------------------------------------------------------------#
 if not sources:
     raise Exception("Source paths are not provided.")
 
@@ -108,34 +132,37 @@ number_wells = params["number_wells"]
 if 'well_clicked' not in st.session_state:
     st.session_state['well_clicked'] = []
 
-files_compounds = [f for f in os.listdir(folder_data) if f.endswith("compounds.json")]
-if files_compounds :
+files_compounds = [f for f in os.listdir(
+    folder_data) if f.endswith("compounds.json")]
+if files_compounds:
     # Load the file and display its contents
     file_path = os.path.join(folder_data, files_compounds[0])
     with open(file_path, "r") as f:
         st.session_state.compounds = json.load(f)
-else :
+else:
     # Create a dictionary to store  data
     if 'compounds' not in st.session_state:
         st.session_state['compounds'] = []
 
-files_dilutions = [f for f in os.listdir(folder_data) if f.endswith("dilutions.json")]
-if files_dilutions :
+files_dilutions = [f for f in os.listdir(
+    folder_data) if f.endswith("dilutions.json")]
+if files_dilutions:
     file_path = os.path.join(folder_data, files_dilutions[0])
     with open(file_path, "r") as f:
         st.session_state.dilutions = json.load(f)
-else :
+else:
     # Create a dictionary to store  data
     if 'dilutions' not in st.session_state:
         st.session_state['dilutions'] = []
 
-files_plate_layout = [f for f in os.listdir(folder_data) if f.endswith("plate_layout.json")]
-if files_plate_layout :
+files_plate_layout = [f for f in os.listdir(
+    folder_data) if f.endswith("plate_layout.json")]
+if files_plate_layout:
     # Load the file and display its contents
     file_path_plate_layout = os.path.join(folder_data, files_plate_layout[0])
     with open(file_path_plate_layout, "r") as f:
         st.session_state.plate_layout = json.load(f)
-else :
+else:
     if "plate_layout" not in st.session_state:
         st.session_state["plate_layout"] = {}
 
@@ -173,7 +200,7 @@ with st.sidebar:
     @st.fragment
     def fragment_sidebar_function():
         st.write("Microplate")
-        with stylable_container(key="well_button", css_styles = f"""
+        with stylable_container(key="well_button", css_styles=f"""
             button{{
                 display: inline-block;
                 width: {size_button}px;  /* Adjust width and height as needed */
@@ -194,10 +221,9 @@ with st.sidebar:
                 }}
                 """,):
 
-
-
             # Define the structure of the microplate
-            wells = [[f"{chr(65 + row)}{col + 1}" for col in range(COLS)] for row in range(ROWS)]
+            wells = [[f"{chr(65 + row)}{col + 1}" for col in range(COLS)]
+                     for row in range(ROWS)]
             # Loop over the wells and create a grid of buttons
             for row in range(ROWS):
                 cols_object = st.columns(COLS)
@@ -205,25 +231,25 @@ with st.sidebar:
                     well = wells[row][col]
 
                     if well in st.session_state['well_clicked']:
-                        if well in st.session_state['plate_layout'] and st.session_state['plate_layout'][well].get("compound") and st.session_state['plate_layout'][well].get("dilution") :
-                            if cols_object[col].button(f":green[{well}] ✓", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                        if well in st.session_state['plate_layout'] and st.session_state['plate_layout'][well].get("compound") and st.session_state['plate_layout'][well].get("dilution"):
+                            if cols_object[col].button(f":green[{well}] ✓", key=well, help=json.dumps(st.session_state['plate_layout'][well], sort_keys=True)):
                                 st.session_state['well_clicked'].remove(well)
                                 st.rerun(scope="app")
                         elif well in st.session_state['plate_layout']:
-                            if cols_object[col].button(f":green[{well}]", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                            if cols_object[col].button(f":green[{well}]", key=well, help=json.dumps(st.session_state['plate_layout'][well], sort_keys=True)):
                                 st.session_state['well_clicked'].remove(well)
                                 st.rerun(scope="app")
-                        else :
+                        else:
                             if cols_object[col].button(f"**:green[{well}]**", key=well):
                                 st.session_state['well_clicked'].remove(well)
                                 st.rerun(scope="app")
                     else:
                         if well in st.session_state['plate_layout'] and st.session_state['plate_layout'][well].get("compound") and st.session_state['plate_layout'][well].get("dilution"):
-                            if cols_object[col].button(f"{well} ✓", key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                            if cols_object[col].button(f"{well} ✓", key=well, help=json.dumps(st.session_state['plate_layout'][well], sort_keys=True)):
                                 st.session_state['well_clicked'].append(well)
                                 st.rerun(scope="app")
                         elif well in st.session_state['plate_layout']:
-                            if cols_object[col].button(well, key=well, help = json.dumps(st.session_state['plate_layout'][well], sort_keys= True)):
+                            if cols_object[col].button(well, key=well, help=json.dumps(st.session_state['plate_layout'][well], sort_keys=True)):
                                 st.session_state['well_clicked'].append(well)
                                 st.rerun(scope="app")
                         else:
@@ -231,13 +257,12 @@ with st.sidebar:
                                 st.session_state['well_clicked'].append(well)
                                 st.rerun(scope="app")
 
-            st.write(f"All the wells clicked are: {', '.join(st.session_state['well_clicked'])}")
+            st.write(
+                f"All the wells clicked are: {', '.join(st.session_state['well_clicked'])}")
 
     fragment_sidebar_function()
 
-    def reset_wells():
-        st.session_state['well_clicked'] = []
     # Add the reset button
-    st.button("Reset Wells", on_click=reset_wells)
+    st.button("Reset wells selection", on_click=reset_wells)
 
 show_content()

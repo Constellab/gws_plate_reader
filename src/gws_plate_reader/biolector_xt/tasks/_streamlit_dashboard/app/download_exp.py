ae6a5c007c0ea6a19885df4ce3c6660700d1b7ce
Maëva <mbeugin@gencovery.com>
2026-02-27 14:44:21 +0000

    update the pipeline when downloading data from BiolectorXT

--- src/gws_plate_reader/biolector_xt/tasks/_streamlit_dashboard/app/download_exp.py
+++ src/gws_plate_reader/biolector_xt/tasks/_streamlit_dashboard/app/download_exp.py
@@ -1,35 +1,40 @@
-
-from typing import List, Optional
-
 import streamlit as st
-from gws_core import (FrontService, Scenario, ScenarioCreationType,
-                      ScenarioProxy, ScenarioSearchBuilder, ScenarioStatus,
-                      Tag)
-from gws_plate_reader.biolector_xt.tasks.biolector_download_experiment_task import \
-    BiolectorDownloadExperiment
-from gws_plate_reader.biolector_xt_analysis.dashboard.analysis_dashboard import \
-    AnalysisDashboard
-from gws_plate_reader.biolector_xt_data_parser.biolector_xt_data_parser import \
-    BiolectorXTDataParser
+from gws_core import (
+    FrontService,
+    Resource,
+    ResourceSearchBuilder,
+    Scenario,
+    ScenarioCreationType,
+    ScenarioProxy,
+    ScenarioSearchBuilder,
+    ScenarioStatus,
+    Tag,
+)
+
+from gws_plate_reader.biolector_xt.tasks.biolector_download_experiment_task import (
+    BiolectorDownloadExperiment,
+)
+from gws_plate_reader.cell_culture_app_core.generate_constellab_bioprocess_dashboard import (
+    GenerateConstellabBioprocessDashboard,
+)
 
 DOWNLOAD_TAG_KEY = "biolector_download"
 
 
 def render_download_exp_main(credentials_name: str, mock_service: bool):
     st.header("Import Biolector experiment")
-    if 'existing_scenario' not in st.session_state:
+    if "existing_scenario" not in st.session_state:
         st.session_state.existing_scenario = None
 
     # Create a form
-    with st.form(key='download_exp'):
-        exp_id = st.text_input(label='Experiment id')
-        submit_button = st.form_submit_button(label='Import biolector experiment result')
+    with st.form(key="download_exp"):
+        exp_id = st.text_input(label="Experiment id")
+        submit_button = st.form_submit_button(label="Import biolector experiment result")
 
     # Handle form submission
     if submit_button:
-
         if not exp_id:
-            st.error('Please provide the biolector experiment id')
+            st.error("Please provide the biolector experiment id")
             return
 
         # check if the biolector experiment was already downloaded
@@ -40,9 +45,17 @@ def render_download_exp_main(credentials_name: str, mock_service: bool):
             scenario = download_experiment(exp_id, credentials_name, mock_service)
             st.session_state.scenario_model = scenario
 
+        # check if the Constellab bioprocess
+        st.session_state.constellab_bioprocess_resource = get_constellab_bioprocess_resource()
+
+        # If there is no app already, create one
+        if not st.session_state.constellab_bioprocess_resource:
+            create_constellab_app()
+            st.session_state.constellab_bioprocess_resource = get_constellab_bioprocess_resource()
+
     if st.session_state.existing_scenario:
-        st.text('The biolector experiment result was already downloaded')
-        if st.button('Force download'):
+        st.text("The biolector experiment result was already downloaded")
+        if st.button("Force download"):
             scenario = download_experiment(exp_id, credentials_name, mock_service)
             st.session_state.scenario_model = scenario
 
@@ -50,19 +63,28 @@ def render_download_exp_main(credentials_name: str, mock_service: bool):
             st.session_state.scenario_model = st.session_state.existing_scenario
 
     # Show a link to open the scenario
-    if 'scenario_model' in st.session_state:
-
+    if "scenario_model" in st.session_state:
         scenario: Scenario = st.session_state.scenario_model
 
         exp_url = FrontService.get_scenario_url(scenario.id)
 
         # show a link with target=_blank to open the scenario in a new tab
-        st.link_button("Open constellab scenario", exp_url)
+        st.link_button("Open Constellab scenario", exp_url)
+    # Show a link to open the constellab bioprocess if it exists
+    if "constellab_bioprocess_resource" in st.session_state:
+        constellab_bioprocess_url = FrontService.get_resource_url(
+            st.session_state.constellab_bioprocess_resource.id
+        )
+
+        st.link_button(
+            "Open Constellab Bioprocess",
+            constellab_bioprocess_url,
+        )
 
 
-def download_experiment(biolector_exp_id: str,
-                        credentials_name: str,
-                        mock_service: bool) -> Scenario:
+def download_experiment(
+    biolector_exp_id: str, credentials_name: str, mock_service: bool
+) -> Scenario:
     """
     Method that creates a scenario to download the biolector experiment data.
 
@@ -74,41 +96,36 @@ def download_experiment(biolector_exp_id: str,
 
     st.session_state.scenario_model = None
 
-    with st.spinner('Downloading experiment file'):
-        scenario = ScenarioProxy(title=f"Download Biolector experiment {biolector_exp_id}",
-                                 creation_type=ScenarioCreationType.MANUAL)
+    with st.spinner("Downloading experiment file"):
+        scenario = ScenarioProxy(
+            title=f"Download Biolector experiment {biolector_exp_id}",
+            creation_type=ScenarioCreationType.MANUAL,
+        )
         scenario.add_tag(Tag(DOWNLOAD_TAG_KEY, biolector_exp_id, is_propagable=True))
         protocol = scenario.get_protocol()
 
-        download_task = protocol.add_task(BiolectorDownloadExperiment, 'download', {
-            'experiment_id': biolector_exp_id,
-            'credentials': credentials_name,
-            'mock_service': mock_service
-        })
+        download_task = protocol.add_task(
+            BiolectorDownloadExperiment,
+            "download",
+            {
+                "experiment_id": biolector_exp_id,
+                "credentials": credentials_name,
+                "mock_service": mock_service,
+            },
+        )
 
-        protocol.add_output('result', download_task.get_output_port('result'))
-        protocol.add_output('raw_data', download_task.get_output_port('raw_data'), False)
-
-        biolector_parser = protocol.add_task(BiolectorXTDataParser, 'biolector_parser')
-        protocol.add_output('parsed_data_tables', biolector_parser.get_output_port('parsed_data_tables'))
-        protocol.add_connector(download_task.get_output_port('result'), biolector_parser.get_input_port('raw_data'))
-        protocol.add_connector(download_task.get_output_port('raw_data'), biolector_parser.get_input_port('folder_metadata'))
-
-        streamlit_generator = protocol.add_task(AnalysisDashboard, 'streamlit_generator')
-
-        protocol.add_connector(biolector_parser.get_output_port('parsed_data_tables'), streamlit_generator.get_input_port('parsed_data'))
-
-        protocol.add_output('streamlit_app', streamlit_generator.get_output_port('streamlit_app'))
+        protocol.add_output("result", download_task.get_output_port("result"))
+        protocol.add_output("raw_data", download_task.get_output_port("raw_data"), False)
 
         try:
             scenario.run()
         except Exception as e:
-            st.error(f'An error occurred while downloading the experiment : {str(e)}')
+            st.error(f"An error occurred while downloading the experiment : {str(e)}")
 
         return scenario.refresh().get_model()
 
 
-def get_biolector_download_experiment(biolector_exp_id: str) -> Optional[Scenario]:
+def get_biolector_download_experiment(biolector_exp_id: str) -> Scenario | None:
     """Method to retrieve the downloader scenario for a biolector exp id
 
     :param biolector_exp_id: _description_
@@ -121,9 +138,62 @@ def get_biolector_download_experiment(biolector_exp_id: str) -> Optional[Scenari
     search_builder.add_tag_filter(Tag(DOWNLOAD_TAG_KEY, biolector_exp_id))
     search_builder.add_expression(Scenario.status == ScenarioStatus.SUCCESS)
 
-    scenarios: List[Scenario] = list(search_builder.build_search())
+    scenarios: list[Scenario] = list(search_builder.build_search())
 
     if len(scenarios) > 0:
         return scenarios[0]
 
     return None
+
+
+def get_constellab_bioprocess_resource() -> Resource | None:
+    """Method to retrieve the Constellab bioprocess resource linked to the downloaded biolector experiment
+
+    :return: resource model of the Constellab bioprocess
+    :rtype: Resource | None
+    """
+
+    search_builder = ResourceSearchBuilder()
+    search_builder.add_tag_filter(
+        Tag(
+            GenerateConstellabBioprocessDashboard.TAG_APP,
+            GenerateConstellabBioprocessDashboard.TAG_APP_CONSTELLAB_BIOPROCESS_DASHBOARD,
+            is_propagable=False,
+        )
+    )
+
+    resources: list[Resource] = list(search_builder.build_search())
+
+    if len(resources) > 0:
+        return resources[0]
+
+    return None
+
+
+def create_constellab_app() -> None:
+    """Method to create a Constellab app scenario for the downloaded biolector experiment
+
+    :return: resource model of the created Constellab app
+    :rtype: Resource
+    """
+
+    scenario = ScenarioProxy(
+        title="Constellab Bioprocess app",
+        creation_type=ScenarioCreationType.MANUAL,
+    )
+
+    protocol = scenario.get_protocol()
+
+    app_task = protocol.add_task(
+        GenerateConstellabBioprocessDashboard,
+        "constellab_bioprocess_task",
+    )
+
+    protocol.add_output("constellab_bioprocess_resource", app_task.get_output_port("dashboard"))
+
+    try:
+        scenario.run()
+    except Exception as e:
+        st.error(f"An error occurred while creating the Constellab app : {str(e)}")
+
+    scenario.refresh()
--- src/gws_plate_reader/biolector_xt/tasks/biolector_download_experiment_task.py
+++ src/gws_plate_reader/biolector_xt/tasks/biolector_download_experiment_task.py
@@ -1,61 +1,91 @@
-
-
 import os
 
-from gws_core import (BoolParam, ConfigParams, ConfigSpecs,
-                      CredentialsDataOther, CredentialsParam, CredentialsType,
-                      File, FileHelper, Folder, InputSpecs, OutputSpec,
-                      OutputSpecs, StrParam, Table, TableImporter, Task,
-                      TaskInputs, TaskOutputs, TypingStyle, ZipCompress,
-                      task_decorator)
-from gws_plate_reader.biolector_xt.biolector_xt_mock_service import \
-    BiolectorXTMockService
-from gws_plate_reader.biolector_xt.biolector_xt_service import \
-    BiolectorXTService
-from gws_plate_reader.biolector_xt.biolector_xt_service_i import \
-    BiolectorXTServiceI
-from gws_plate_reader.biolector_xt.biolector_xt_types import \
-    CredentialsDataBiolector
+from gws_core import (
+    BoolParam,
+    ConfigParams,
+    ConfigSpecs,
+    CredentialsDataOther,
+    CredentialsParam,
+    CredentialsType,
+    File,
+    FileHelper,
+    Folder,
+    InputSpecs,
+    OutputSpec,
+    OutputSpecs,
+    StrParam,
+    Table,
+    TableImporter,
+    Task,
+    TaskInputs,
+    TaskOutputs,
+    TypingStyle,
+    ZipCompress,
+    task_decorator,
+)
+
+from gws_plate_reader.biolector_xt.biolector_xt_mock_service import BiolectorXTMockService
+from gws_plate_reader.biolector_xt.biolector_xt_service import BiolectorXTService
+from gws_plate_reader.biolector_xt.biolector_xt_service_i import BiolectorXTServiceI
+from gws_plate_reader.biolector_xt.biolector_xt_types import CredentialsDataBiolector
 
 
-@task_decorator(unique_name="BiolectorDownloadExperiment",
-                short_description="Download the reuslt of an experiment from Biolector XT",
-                style=TypingStyle.community_icon("bioreactor"))
+@task_decorator(
+    unique_name="BiolectorDownloadExperiment",
+    short_description="Download the reuslt of an experiment from Biolector XT",
+    style=TypingStyle.community_icon("bioreactor"),
+)
 class BiolectorDownloadExperiment(Task):
-
-    config_specs: ConfigSpecs = ConfigSpecs({
-        'experiment_id': StrParam(human_name="Experiment ID",
-                                  short_description="The ID of the BiolectorXT experiment to download"),
-        'credentials': CredentialsParam(credentials_type=CredentialsType.OTHER),
-        'mock_service': BoolParam(human_name="Mock Service",
-                                  short_description="Use the mock service to simulate the interaction with Biolector XT",
-                                  default_value=False, visibility="private")
-    })
+    config_specs: ConfigSpecs = ConfigSpecs(
+        {
+            "experiment_id": StrParam(
+                human_name="Experiment ID",
+                short_description="The ID of the BiolectorXT experiment to download",
+            ),
+            "credentials": CredentialsParam(credentials_type=CredentialsType.OTHER),
+            "mock_service": BoolParam(
+                human_name="Mock Service",
+                short_description="Use the mock service to simulate the interaction with Biolector XT",
+                default_value=False,
+                visibility="private",
+            ),
+        }
+    )
     input_specs: InputSpecs = InputSpecs()
-    output_specs: OutputSpecs = OutputSpecs({
-        'result': OutputSpec(Table, human_name="Result Table",
-                             short_description="The result of the experiment in a table format"),
-        'raw_data': OutputSpec(Folder, human_name="Raw Data Folder",
-                               short_description="The unzipped raw data of the experiment")
-    })
+    output_specs: OutputSpecs = OutputSpecs(
+        {
+            "result": OutputSpec(
+                Table,
+                human_name="Result Table",
+                short_description="The result of the experiment in a table format",
+            ),
+            "raw_data": OutputSpec(
+                Folder,
+                human_name="Raw Data Folder",
+                short_description="The unzipped raw data of the experiment",
+            ),
+        }
+    )
 
     zip_path = None
 
     def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
         # Get the experiment ID
-        experiment_id: str = params.get_value('experiment_id')
+        experiment_id: str = params.get_value("experiment_id")
         experiment_id = experiment_id.strip()
 
         # Set { at the beginning and } at the end of the experiment ID if not present
-        if not experiment_id.startswith('{'):
+        if not experiment_id.startswith("{"):
             self.log_info_message("Adding missing '{' at the beginning of the experiment ID")
-            experiment_id = '{' + experiment_id
+            experiment_id = "{" + experiment_id
 
-        if not experiment_id.endswith('}'):
+        if not experiment_id.endswith("}"):
             self.log_info_message("Adding missing '}' at the end of the experiment ID")
-            experiment_id = experiment_id + '}'
+            experiment_id = experiment_id + "}"
 
-        service = self.get_service(params.get_value('credentials'), params.get_value('mock_service'))
+        service = self.get_service(
+            params.get_value("credentials"), params.get_value("mock_service")
+        )
 
         # download experiment
         self.log_info_message(f"Downloading experiment {experiment_id} from Biolector XT")
@@ -70,7 +100,7 @@ class BiolectorDownloadExperiment(Task):
         # find the csv file in the unzipped folder, no recursion
         csv_file = None
         for file in os.listdir(tmp_dir):
-            if file.endswith('.csv'):
+            if file.endswith(".csv"):
                 csv_file = os.path.join(tmp_dir, file)
                 break
 
@@ -79,24 +109,29 @@ class BiolectorDownloadExperiment(Task):
 
         self.log_info_message(f"Importing csv file: {csv_file}")
 
-        table = TableImporter.call(File(csv_file), {
-            'file_format': 'csv',
-            'delimiter': ';',
-            'header': 0,
-            'format_header_names': True,
-            'index_column': -1,
-        })
+        table = TableImporter.call(
+            File(csv_file),
+            {
+                "file_format": "csv",
+                "delimiter": ";",
+                "header": 0,
+                "format_header_names": True,
+                "index_column": -1,
+            },
+        )
 
         folder = Folder(tmp_dir)
         folder.name = f"Biolector raw data {experiment_id}"
-        return {'result': table, 'raw_data': folder}
+        return {"result": table, "raw_data": folder}
 
     def run_after_task(self) -> None:
         super().run_after_task()
         if self.zip_path:
             FileHelper.delete_file(self.zip_path)
 
-    def get_service(self, credentials: CredentialsDataOther, mock_service: bool) -> BiolectorXTServiceI:
+    def get_service(
+        self, credentials: CredentialsDataOther, mock_service: bool
+    ) -> BiolectorXTServiceI:
         if mock_service:
             return BiolectorXTMockService(self.message_dispatcher)
         else:
